0f6fe4a068b2c7465671324204bb13a2f9e20ee6
Dr-Phi <eliasrojaslon@gmail.com>
2023-04-06 10:36:44 -0500

    Refactoring some small stuff

--- index.js
+++ index.js
@@ -6,30 +6,27 @@ function getDataFromJson() {
   fetch(URL)
     .then((res) => res.json())
     .then((objs) => loadObjs(objs))
-    .finally(console.log("Finished"))
     .catch(handleErr);
+}
+
+function loadObjs(objs) {
   let i = 0;
-  function loadObjs(objs) {
-    const divResults = document.querySelector("#results");
-    let toDOM = `
-    
-    ${objs
-      .map((obj) => {++i;
-        return `
+  const divResults = document.querySelector("#results");
+  let toDOM = `${objs
+    .map((obj) => {
+      ++i;
+      return `
         <div class="card card${i}">
           <object data=${obj.icon} class="icon"></object>
           <p>${obj.category}</p>
           <div class='score'>
             <p><span>${obj.score}</span>/ 100</p>
           </div>
-        </div>
-      `;
-      })
-      .join("")}
-    `;
-    console.log(toDOM);
-    divResults.innerHTML = toDOM;
-  }
+        </div>`;
+    })
+    .join("")}`;
+  console.log(toDOM);
+  divResults.innerHTML = toDOM;
 }
 
 getDataFromJson()
